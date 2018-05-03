3aa04db9feeea728ca3a5e38f62c22533ca4c30a
TigraCoin <tnashnas@gmail.com>
2018-05-03 16:51:42 -0700

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define MillionairesExchangeCoinClub-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "MillionairesExchangeCoinClubURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
